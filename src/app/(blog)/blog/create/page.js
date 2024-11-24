4ecc3fd8a2d703f0765cec7c242832812489cf8f
mlnjun <rhalswns098@gmail.com>
2024-11-24 19:27:51 +0900

    블로그 생성 페이지 디자인 완료 Editor, editorBubble 디자인 필요

--- src/app/(blog)/blog/create/page.js
+++ src/app/(blog)/blog/create/page.js
@@ -3,11 +3,46 @@ import Editor from "@/components/Editor";
 const page = () => {
   return (
     <div className="flex flex-col mt-4 mx-auto max-w-[1080px]">
-      <h1 className="h3">블로그 생성</h1>
-
       {/* 콘텐츠 영역 */}
       <div className="flex flex-col">
-        <Editor></Editor>
+        <input
+          className="h2 my-1.5 p-2 w-full bg-white rounded-lg min-h-[36px] max-md:max-w-full box-border border-[2px] border-transparent focus:border-primary focus:border-opacity-30 focus:outline-none"
+          type="text"
+          placeholder="제목"
+        />
+
+        <hr />
+
+        <div className="flex justify-between items-center">
+          <input
+            className="body-1 mt-2 p-2 w-[50%] bg-white rounded-lg min-h-[36px] max-md:max-w-full box-border border-[2px] border-transparent focus:border-primary focus:border-opacity-30 focus:outline-none"
+            type="text"
+            placeholder="태그 추가"
+          />
+
+          <select
+            type="text"
+            className="my-2 p-2 w-[33%] bg-white rounded-lg min-h-[36px] max-md:max-w-full box-border border-[2px] border-n-5 focus:border-primary focus:border-opacity-30 focus:outline-none"
+          >
+            <option>카테고리</option>
+          </select>
+        </div>
+        <div className="flex gap-1.5 mt-1 mb-2">
+          <div className="font-semibold text-white bg-primary bg-opacity-80 p-2 border-[0.25px] rounded-lg">
+            JavaScript
+          </div>
+          <div className="font-semibold text-white bg-primary bg-opacity-80 p-2 border-[0.25px] rounded-lg">
+            Node.js
+          </div>
+        </div>
+
+        <hr />
+
+        <hr />
+
+        <div className="mt-2">
+          <Editor />
+        </div>
       </div>
     </div>
   );
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,7 +2,7 @@
 "use client"; // 클라이언트 컴포넌트로 설정
 
 import React from "react";
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
@@ -27,7 +27,35 @@ const Editor = () => {
   return (
     <div>
       <EditorMenuBar editor={editor} />
-      <EditorContent editor={editor} className="p-1" />
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div className="bubble-menu">
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+            >
+              Bold
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+            >
+              Italic
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+            >
+              Strike
+            </button>
+          </div>
+        </BubbleMenu>
+      )}
+      <EditorContent
+        editor={editor}
+        className="p-1 h-[calc(100vh-480px)] overflow-y-scroll"
+        onClick={() => editor.commands.focus()}
+      />
       <button onClick={() => console.log(editor.getHTML())}>게시</button>
     </div>
   );
